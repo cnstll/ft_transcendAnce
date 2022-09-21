848642167a829559b513d0d80498b7e38a7ef878
COTHSC <thierryscully.colomban@gmail.com>
2022-09-21 16:09:20 +0200

    adding cleanup script to remove old volumes and containers and .dockerignore to prevent node_modules from being bound to containers

---
+++ cleanup_containers.sh
@@ -0,0 +1,5 @@
+docker stop $(docker ps -qa); 
+docker rm $(docker ps -qa); 
+docker rmi -f $(docker images -qa);
+docker volume rm $(docker volume ls -q);
+docker network rm $(docker network ls -q) 2>/dev/null;
