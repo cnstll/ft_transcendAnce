983a48d40732eaae597722999448e4413879340f
COTHSC <thierryscully.colomban@gmail.com>
2022-09-22 10:56:57 +0200

    Backend container now waits for db to be up before attempting a migration

--- backend/entrypoint_scripts/start.sh
+++ backend/entrypoint_scripts/start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
+/home/entrypoint_scripts/wait-for-it.sh  db:5432
 npm install -ci;
-npx prisma migrate dev --name updatedb; 
-npm run start:dev;
+npx prisma migrate dev --name updatedb && npm run start:dev;
