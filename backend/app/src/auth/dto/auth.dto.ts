dd740e5b982211312e073b788ce6ce32d193af1c
COTHSC <thierryscully.colomban@gmail.com>
2022-09-25 19:46:38 +0200

    added functionality to create users from backend

--- backend/app/src/auth/dto/auth.dto.ts
+++ backend/app/src/auth/dto/auth.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
   @IsString()
   @IsNotEmpty()
-  name: String;
+  name: string;
 }
