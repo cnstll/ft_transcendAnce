9f4ccfb7bf44e0d868dac50c86687a3f2c725a97
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-26 19:15:46 +0200

    fix(front): add responsiveness to fonts

--- frontend/app/src/components/SignIn.tsx
+++ frontend/app/src/components/SignIn.tsx
@@ -8,7 +8,7 @@ function SignIn() {
         <a href="https://google.com" className="bg-blue hover:bg-dark-blue text-white font-bold py-2 px-4 w-2/12 h-16 rounded-2xl">
           <div className="flex justify-center items-center gap-2">
             <img src={logo} alt="Logo" className="object-contain w-2/12 h-12"/>
-            <p className="text-2xl"> SIGN IN </p>
+            <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> SIGN IN </p>
           </div>
         </a>
       </div>
