601fcc640f43f5ed192b9df0e001c2fef4cbdb81
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-26 14:52:41 +0200

    fix(front): use typescript instead of javascript

---
+++ frontend/app/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import SignIn from './components/SignIn';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <SignIn />
+);
