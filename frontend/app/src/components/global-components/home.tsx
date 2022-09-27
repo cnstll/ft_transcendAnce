4fbadd276aaa19c65911911821b31e1c01e8316d
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-27 17:56:59 +0200

    feat(front): add banner

--- frontend/app/src/components/global-components/home.tsx
+++ frontend/app/src/components/global-components/home.tsx
@@ -1,4 +1,4 @@
-import HomeBanner from '../section-components/home-banner';
+import Banner from '../section-components/banner';
 import backgroundGeneral from "../../img/disco2.png";
 import { Link } from 'react-router-dom';
 
@@ -6,18 +6,18 @@ import { Link } from 'react-router-dom';
 function Home () {
     return (
     <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundGeneral})` }}>
-        <HomeBanner />
+        <Banner text="TRANSCENDANCE 🕺"/>
         <div className="flex flex-col h-screen justify-center items-center gap-10">
             <div className="bg-blue hover:bg-dark-blue text-white py-2 px-4 w-2/12 h-16 rounded-2xl">
                 <Link to ="/play">
-                    <div className="flex justify-center items-center gap-2">
+                    <div className="flex justify-center gap-2">
                         <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> PLAY </p>
                     </div>
                 </Link>
             </div>
             <div className="bg-blue hover:bg-dark-blue text-white py-2 px-4 w-2/12 h-16 rounded-2xl">
                 <Link to ="/chat">
-                    <div className="flex justify-center items-center gap-2">
+                    <div className="flex justify-center gap-2">
                         <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> CHAT </p>
                     </div>
                 </Link>
--- frontend/app/src/components/section-components/banner.tsx
+++ frontend/app/src/components/section-components/banner.tsx
@@ -3,15 +3,38 @@ import { faHouse } from '@fortawesome/free-solid-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
+import {useState} from 'react';
 
-
-function Banner ()
+function UserInfo()
 {
+    return (
+        <div className="w-8/12 h-full bg-purple text-white text-lg sm:text-xs md:text-sm font-bold flex flex-col">
+            <Link to="/profile">
+                <p className="flex justify-center">Profile</p>
+            </Link>
+            <Link to="/ranking">
+                <p className="flex justify-center">Ranking</p>
+            </Link>
+            <Link to="/sign-in">
+                <p className="flex justify-center">Log Out</p>
+            </Link>
+        </div>
+    )
+}
+
+function Banner (props)
+{
+    const [isShown, setIsShown] = useState(false);
+
+    const showInfo = event => {
+    setIsShown(current => !current);
+    }
+
     return (
         <div className="flex flex-row px-5 py-5">
             <Link to="/">
-                <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl text-white font-bold">
-                    <FontAwesomeIcon icon={faHouse} />
+                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white font-bold">
+                    {props.text}
                 </h1>
             </Link>
             <div className="pt-2 relative mx-auto text-gray-600">
@@ -21,9 +44,14 @@ function Banner ()
                         <FontAwesomeIcon icon={faMagnifyingGlass}/>
                     </button>
             </div>
-            <div className="text-sm sm:text-xl md:text-2xl lg:text-3xl flex flex-row gap-2 place-items-end">
-                <img className="w-12 h-12 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar"/>
-                <FontAwesomeIcon icon={faChevronDown} className="text-white font-bold"/>
+            <div className="flex flex-col gap-2">
+                <div className="text-sm sm:text-xl md:text-2xl lg:text-3xl flex flex-row gap-2 place-items-end">
+                    <img className="w-4/12 h-4/12 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-5.jpg" alt="Rounded avatar"/>
+                    <button onClick={showInfo} className="text-white font-bold">
+                        <FontAwesomeIcon icon={faChevronDown} />
+                    </button>
+                </div>
+                {isShown && <UserInfo/> }
             </div>
         </div>
     );
