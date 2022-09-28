c4bc39da7fe15f42dd1ed92cc85cbf1e3d6a20aa
COTHSC <thierryscully.colomban@gmail.com>
2022-09-28 15:31:03 +0200

    fixed format

--- backend/app/src/auth/auth.controller.spec.ts
+++ backend/app/src/auth/auth.controller.spec.ts
@@ -1,23 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
-import * as request from 'supertest';
 import { AuthController } from './auth.controller';
 
 describe('MyService', () => {
   let controller: AuthController;
   const mockPrismaService = {
-    userCreate: jest.fn(dto => {
+    userCreate: jest.fn((dto) => {
       return {
-        id: "this",
-        ...dto
-      }
-    })
+        id: 'this',
+        ...dto,
+      };
+    }),
   };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
       providers: [AuthService],
-    }).overrideProvider(AuthService).useValue(mockPrismaService).compile();
+    })
+      .overrideProvider(AuthService)
+      .useValue(mockPrismaService)
+      .compile();
     controller = module.get<AuthController>(AuthController);
   });
 
@@ -25,12 +27,9 @@ describe('MyService', () => {
     expect(controller).toBeDefined();
   });
   it('should create a user', () => {
-    expect(controller.signup({ name: 'a' })).toEqual(
-      {
-        id: expect.any(String),
-        name: 'a'
-      }
-    );
+    expect(controller.signup({ name: 'a' })).toEqual({
+      id: expect.any(String),
+      name: 'a',
+    });
   });
-
 });
