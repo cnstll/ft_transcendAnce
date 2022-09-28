2e139d0e279f6fcdc54ba34bfe5cfa2e6b3973de
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-28 18:05:13 +0200

    refactor(front): add components for button and background

--- frontend/app/src/components/global-components/home.tsx
+++ frontend/app/src/components/global-components/home.tsx
@@ -1,25 +1,28 @@
 import Banner from '../section-components/banner';
-import backgroundGeneral from "../../img/disco2.png";
+import BackgroundGeneral from "../../img/disco2.png";
 import { Link } from 'react-router-dom';
-
+import Background from '../section-components/background';
+import Button from '../section-components/button';
 
 function Home () {
     return (
-    <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundGeneral})` }}>
-        <Banner text="TRANSCENDANCE 🕺"/>
-        <div className="flex flex-col h-screen justify-center items-center gap-10">
-            <Link to ="/play">
-                <div className="bg-blue hover:bg-dark-blue text-white p-4 w-40 h-16 rounded-2xl text-center">
-                    <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> PLAY </p>
+        <div>
+            <Background background={BackgroundGeneral}>
+                <Banner text="TRANSCENDANCE 🕺"/>
+                <div className="flex flex-col h-screen justify-center items-center gap-10">
+                    <Link to ="/play">
+                        <Button>
+                            <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> PLAY </p>
+                        </Button>
+                    </Link>
+                    <Link to ="/chat">
+                        <Button>
+                            <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> CHAT </p>
+                        </Button>
+                    </Link>
                 </div>
-            </Link>
-            <Link to ="/chat">
-                <div className="bg-blue hover:bg-dark-blue text-white p-4 w-40 h-16 rounded-2xl text-center">
-                    <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> CHAT </p>
-                </div>
-            </Link>
+            </Background>
         </div>
-    </div>
     );
 }
 
--- frontend/app/src/components/global-components/ranking.tsx
+++ frontend/app/src/components/global-components/ranking.tsx
@@ -1,12 +1,15 @@
 import Banner from '../section-components/banner';
-import backgroundGeneral from "../../img/disco2.png";
+import BackgroundGeneral from "../../img/disco2.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import Background from '../section-components/background';
 
 function Ranking () {
     return (
-    <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundGeneral})` }}>
-        <Banner text = < FontAwesomeIcon icon={faHouse} /> />
+    <div>
+        <Background background={BackgroundGeneral}>
+            <Banner text = < FontAwesomeIcon icon={faHouse} /> />
+        </Background>
     </div>
     );
 }
