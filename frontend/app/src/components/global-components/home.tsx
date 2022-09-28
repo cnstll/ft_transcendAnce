be2958f347e5f78c1ee923df19cb294f64e84cfc
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-28 13:03:16 +0200

    fix(front): fix issue on banner

--- frontend/app/src/components/global-components/home.tsx
+++ frontend/app/src/components/global-components/home.tsx
@@ -8,20 +8,16 @@ function Home () {
     <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${backgroundGeneral})` }}>
         <Banner text="TRANSCENDANCE 🕺"/>
         <div className="flex flex-col h-screen justify-center items-center gap-10">
-            <div className="bg-blue hover:bg-dark-blue text-white py-2 px-4 w-2/12 h-16 rounded-2xl">
-                <Link to ="/play">
-                    <div className="flex justify-center gap-2">
-                        <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> PLAY </p>
-                    </div>
-                </Link>
-            </div>
-            <div className="bg-blue hover:bg-dark-blue text-white py-2 px-4 w-2/12 h-16 rounded-2xl">
-                <Link to ="/chat">
-                    <div className="flex justify-center gap-2">
-                        <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> CHAT </p>
-                    </div>
-                </Link>
-            </div>
+            <Link to ="/play">
+                <div className="bg-blue hover:bg-dark-blue text-white p-4 w-40 h-16 rounded-2xl text-center">
+                    <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> PLAY </p>
+                </div>
+            </Link>
+            <Link to ="/chat">
+                <div className="bg-blue hover:bg-dark-blue text-white p-4 w-40 h-16 rounded-2xl text-center">
+                    <p className="text-xs sm:text-xs md:text-lg lg:text-2xl"> CHAT </p>
+                </div>
+            </Link>
         </div>
     </div>
     );
--- frontend/app/src/components/global-components/sign-in.tsx
+++ frontend/app/src/components/global-components/sign-in.tsx
@@ -4,10 +4,10 @@ import logo from "../../img/42_logo.png";
 function SignIn() {
   return (
     <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
-      <div className="flex h-screen justify-center items-center">
-        <a href="https://google.com" className="bg-blue hover:bg-dark-blue py-2 px-4 w-2/12 h-16 rounded-2xl">
-          <div className="flex justify-center items-center gap-2">
-            <img src={logo} alt="Logo" className="object-contain w-2/12 h-12"/>
+      <div className="flex flex-col h-screen justify-center items-center gap-10">
+        <a href="https://google.com" className="bg-blue hover:bg-dark-blue w-2/12 h-16 rounded-2xl p-4">
+          <div className="flex flex-row justify-center gap-2">
+            <img src={logo} alt="Logo" className="w-2/12"/>
             <p className="text-xs sm:text-xs md:text-lg lg:text-2xl text-white font-bold"> SIGN IN</p>
           </div>
         </a>
