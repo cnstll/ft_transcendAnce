1696c9f698d609094760ff166afc93d9e00157d6
Lea Lescure <lea.lescure@hotmail.fr>
2022-09-28 18:05:13 +0200

    refactor(front): add components for button and background

--- frontend/app/src/components/global-components/sign-in.tsx
+++ frontend/app/src/components/global-components/sign-in.tsx
@@ -1,17 +1,23 @@
-import background from "../../img/disco.png";
+import BackgroundSignin from "../../img/disco.png";
 import logo from "../../img/42_logo.png";
+import Background from '../section-components/background';
+import Button from "../section-components/button";
 
 function SignIn() {
   return (
-    <div className="h-screen bg-cover bg-no-repeat" style={{ backgroundImage: `url(${background})` }}>
-      <div className="flex flex-col h-screen justify-center items-center gap-10">
-        <a href="https://google.com" className="bg-blue hover:bg-dark-blue w-2/12 h-16 rounded-2xl p-4">
-          <div className="flex flex-row justify-center gap-2">
-            <img src={logo} alt="Logo" className="w-2/12"/>
-            <p className="text-xs sm:text-xs md:text-lg lg:text-2xl text-white font-bold"> SIGN IN</p>
-          </div>
-        </a>
-      </div>
+    <div>
+      <Background background={BackgroundSignin}>
+        <div className="flex flex-col h-screen justify-center items-center gap-10">
+          <a href="https://google.com">
+            <Button>
+              <div className="flex flex-row justify-center gap-2">
+                <img src={logo} alt="Logo" className="w-2/12"/>
+                <p className="text-xs sm:text-xs md:text-lg lg:text-2xl text-white font-bold"> SIGN IN</p>
+              </div>
+            </Button>
+          </a>
+        </div>
+      </Background>
     </div>
   );
 }
