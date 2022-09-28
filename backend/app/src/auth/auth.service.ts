1c5f237fdd115db5513d781e5f8ac321abc8ca3d
COTHSC <53475390+COTHSC@users.noreply.github.com>
2022-09-28 16:47:41 +0200

    2 testing create tests backbone (#4)
    * first backend e2e tests
    
    * first backend e2e tests
    
    * basic infrastructure done for tests, scripts in package.json will lauch appropriate containers for test or dev
    
    * clean up and hone test infra
    
    * added script to run studio in the test env
    
    * modified test to provide coverage and added helper scripts to package.json
    
    * fixed format
    
    * Running backend tests does not create migration
    
    Co-authored-by: cnstll <calle@student.42.fr>
    Co-authored-by: cnstll <67589684+cnstll@users.noreply.github.com>

--- backend/app/src/auth/auth.service.ts
+++ backend/app/src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
+
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
