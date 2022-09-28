a5d91938853124446ba65744bd7586e2cb40458e
COTHSC <thierryscully.colomban@gmail.com>
2022-09-28 15:29:04 +0200

    modified test to provide coverage and added helper scripts to package.json

--- backend/app/src/auth/auth.service.ts
+++ backend/app/src/auth/auth.service.ts
@@ -4,7 +4,7 @@ import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async userCreate(dto: AuthDto) {
     const user = await this.prisma.users.create({
