3720258262262b74698162a96e9a5cfd27070f89
Lea Lescure <lea.lescure@hotmail.fr>
2022-10-03 19:15:41 +0200

    fix(front): fix cross in search bar while typing

--- frontend/app/src/components/section-components/banner.tsx
+++ frontend/app/src/components/section-components/banner.tsx
@@ -42,7 +42,7 @@ function Banner (props)
             <div className="relative">
                 <input className=" bg-white h-10 w-24 sm:w-36 sm:h-11 md:w-40 md:h-12 lg:w-56 lg:h-14 xl:w-56 xl:h-14
                     px-2 py-2 rounded-lg text-[8px] sm:text-xs md:text-xs lg:text-sm focus:outline-none relative"
-                    type="search" name="search" placeholder="Search player"/>
+                    type="text" name="search" placeholder="Search player"/>
                     <button type="submit" className="absolute top-4 right-2 text-[8px] sm:text-xs md:text-xs lg:text-sm">
                         <FontAwesomeIcon icon={faMagnifyingGlass}/>
                     </button>
