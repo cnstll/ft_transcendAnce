dc279c2795881ac638fb457125a1583b1b621905
Léa Lescure <64603276+llescure@users.noreply.github.com>
2022-10-05 12:55:22 +0200

    fix(front): fix responsive for home page and profile (#55)
    * fix(front): fix responsive for home page and profile
    
    * fix(front): fix cross in search bar while typing
    
    * fix(front): fix background, button center, responsive with wrap on profile
    
    * fix(back): fix lint error
    
    * fix(back): fix eslint error
    
    * Lele youre a bouletos but I love you
    
    * fix(front): fix typo on banner
    
    * fix(banner): fix double banner

--- frontend/app/src/components/section-components/center-box.tsx
+++ frontend/app/src/components/section-components/center-box.tsx
@@ -1,6 +1,6 @@
 function CenterBox(props) {
     return (
-        <div className="bg-purple h-[40 rem] w-7/12">
+        <div className="bg-purple h-44 lg:h-96 xl:h-[40rem] xl:w-7/12 lg:w-7/12 md:w-1/2 min-w-[280px] text-xl sm:text-xl md:text-2xl lg:text-3xl">
             {props.children}
         </div>
     );
