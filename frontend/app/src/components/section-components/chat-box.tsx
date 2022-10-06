e0ebba1e71a48512bd1d1c00350315099de320b5
Lea Lescure <lea.lescure@hotmail.fr>
2022-10-06 18:11:12 +0200

    fix(type): fix type in message.tsx

--- frontend/app/src/components/section-components/chat-box.tsx
+++ frontend/app/src/components/section-components/chat-box.tsx
@@ -9,7 +9,7 @@ function ChatBox() {
             h-16 xl:w-7/12 lg:w-7/12 md:w-1/2 min-w-[280px]"> 
             <input className="w-11/12 bg-purple text-white font-bold px-3" type="text"
                 name="sendMessage" placeholder="Type your message here"/>
-            <button className="bg-blue hover:bg-dark-blue text-white p-4 w-1/12
+            <button type="submit" className="bg-blue hover:bg-dark-blue text-white p-4 w-1/12
                 rounded-2xl flex justify-center text-center">
                 <div className="text-xl sm:text-xl md:text-2xl lg:text-3xl">
                     <FontAwesomeIcon icon={faPlay}/>
--- frontend/app/src/components/section-components/message.tsx
+++ frontend/app/src/components/section-components/message.tsx
@@ -1,4 +1,11 @@
-const datas = [
+interface Message {
+    id?: number;
+    message: string;
+    image: string;
+    currentUser?: boolean;
+}
+
+const datas: Message[] = [
     {
         id: 1,
         message: "Who would like to go dancing dancing dancing dancing dancing dancing dancing dancing?",
@@ -37,32 +44,30 @@ const datas = [
     },
 ];
 
-
-
-function OtherUserMessage(props)
+function OtherUserMessage({ image, message }: Message)
 {
     return (
         <div className="flex flex-row items-center gap-4">
             <img className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
-                    src={props.image} alt="Rounded avatar"/>
+                    src={image} alt="Rounded avatar"/>
             <div className="w-2/6 max-w-[30rem] min-w-[150px] min-h-[2rem]
                 p-2 bg-gray-300 rounded-2xl text-center text-xs sm:text-xs md:text-sm lg:text-sm xl:text-sm text-black">
-                {props.message}
+                {message}
             </div>
         </div>    
         )
 }
 
-function CurrentUserMessage(props)
+function CurrentUserMessage({ image, message }: Message)
 {
     return (
         <div className="flex flex-row justify-end items-center gap-4">
             <div className="w-2/6 h-1/6 min-w-[150px] min-h-[2rem]
                 p-2 bg-blue rounded-2xl text-center text-xs sm:text-xs md:text-sm lg:text-sm xl:text-sm">
-                {props.message}
+                {message}
             </div>
             <img className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
-                    src={props.image} alt="Rounded avatar"/>
+                    src={image} alt="Rounded avatar"/>
         </div>
     )
 }
@@ -73,8 +78,8 @@ function Message()
         <div className="p-5 flex flex-col gap-4">
                 {datas.map((data) => 
                     data.currentUser ?
-                    <CurrentUserMessage message={data.message} image={data.image}/>
-                    : <OtherUserMessage message={data.message} image={data.image}/>
+                    <CurrentUserMessage key={data.id} message={data.message} image={data.image}/>
+                    : <OtherUserMessage key={data.id} message={data.message} image={data.image}/>
                 )}
         </div>
     )
