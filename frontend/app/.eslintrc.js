7e494c3ddcf879a2e2cab94ba800b33deeb68eee
Estelle Recuero <estelle-ad@outlook.com>
2022-10-06 16:29:38 +0200

    adding compilation flags to tsconfig + to linter

--- frontend/app/.eslintrc.js
+++ frontend/app/.eslintrc.js
@@ -4,6 +4,15 @@ module.exports = {
     project: 'tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
+    }
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
@@ -20,6 +29,10 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'on',
+    '@typescript-eslint/strict': 'on',
+    '@typescript-eslint/no-unsused-parameters': 'on',
+    '@typescript-eslint/no-unused-returns': 'on',
+    '@typescript-eslint/no-fall-through-cases-in-switch': 'on',
   },
 };
