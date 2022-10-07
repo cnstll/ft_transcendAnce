1c39da7c80f20d2ca7e3dcf2cc42fcd9f139b77a
Léa Lescure <64603276+llescure@users.noreply.github.com>
2022-10-07 15:36:34 +0200

    61 us chat message front (#67)
    * feat(message): add distinction between current user message and other user messages
    
    * feat(message): channel message but responsive is not working
    
    * fix(responsive): reponsive on chat fixed
    
    * fix(type): fix type in message.tsx

--- frontend/app/src/components/section-components/side-box.tsx
+++ frontend/app/src/components/section-components/side-box.tsx
@@ -1,6 +1,7 @@
 function SideBox(props) {
     return (
-        <div className="bg-purple text-white p-5 h-44 lg:h-96 xl:h-[40rem] xl:w-2/12 lg:w-2/12 md:w-1/2 min-w-[280px] text-xl sm:text-xl md:text-2xl lg:text-3xl">
+        <div className="bg-purple text-white p-5 h-44 lg:h-96 xl:h-[40rem] xl:w-2/12 lg:w-2/12 md:w-1/2 min-w-[280px]
+            text-xl sm:text-xl md:text-2xl lg:text-3xl overflow-y-auto">
             {props.children}
         </div>
     );
