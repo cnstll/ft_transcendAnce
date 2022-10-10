64209992568da0771210d95800767996a0afa7a8
Lea Lescure <lea.lescure@hotmail.fr>
2022-10-10 12:14:35 +0200

    feat(ranking): best layout with table compared with other options

--- frontend/app/src/components/global-components/ranking.tsx
+++ frontend/app/src/components/global-components/ranking.tsx
@@ -49,12 +49,23 @@ function RankingList ({position, image, name, score}: RankingData) {
     return (
         <tr className="h-20">
             {position === 1 ?
-                <td className="flex justify-center pt-5 text-5xl">🏆</td>
-                : <td className="flex justify-center pt-5 text-3xl">{position}</td> }
-            <td><img className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
-                src={image} alt="Rounded avatar"/></td>
-            <td>{name}</td> 
-            <td> < NumericFormat className="flex justify-center text-lg sm:text-lg md:text-xl lg:text-2xl" value={score} thousandSeparator=" " displayType="text"/> </td>
+                <td className="flex justify-center pt-5
+                    text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
+                        🏆
+                </td>
+                : <td className="flex justify-center pt-5 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+                    {position}
+                </td> }
+            <td>
+                <img className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
+                    src={image} alt="Rounded avatar"/>
+            </td>
+            <td>
+                {name}
+            </td> 
+            <td className="flex justify-center">
+                < NumericFormat className="text-lg sm:text-lg md:text-xl lg:text-2xl" value={score} thousandSeparator=" " displayType="text"/>
+            </td>
         </tr>
     );
 }
@@ -64,17 +75,17 @@ function Ranking () {
     <div>
         <Background background={BackgroundGeneral}>
             <Banner text = < FontAwesomeIcon icon={faHouse} /> />
-            <div className="flex justify-center mt-6 ">
+            <div className="flex justify-center mt-6">
                 <OneBox>
                     <h1 className="flex justify-center mt-6 text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold">RANKING</h1>
-                        <table className="m-10 min-w-full">
+                        <table className="min-w-full border-collapse mt-10">
                             <thead>
                                 <tr>
-                                    <td className="w-20">
+                                    <td className="w-10 sm:w-12 md:w-14 lg:w-20">
                                     </td>
-                                    <td className="w-20">
+                                    <td className="w-10 sm:w-12 md:w-14 lg:w-20">
                                     </td>
-                                    <td>
+                                    <td className="w-1/6 sm:w-2/6 md:w-3/6 lg:w-4/6">
                                     </td>
                                     <td className="flex justify-center text-xl sm:text-xl md:text-2xl lg:text-3xl font-bold">
                                         SCORE
