2415025740c11c296ac9826a4bc6773ef6ed00bf
Estelle R <estelle-ad@outlook.com>
2022-10-10 18:38:34 +0200

    fix overflow-auto

--- frontend/app/src/components/section-components/one-box.tsx
+++ frontend/app/src/components/section-components/one-box.tsx
@@ -1,6 +1,6 @@
 function OneBox(props) {
     return (
-        <div className="bg-purple text-white h-[40rem] w-9/12 overflow-scroll">
+        <div className="bg-purple text-white h-[40rem] w-9/12 overflow-auto">
             {props.children}
         </div>
     );
