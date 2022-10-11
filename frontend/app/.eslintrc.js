0418603bb04060e77cfccec1fbbb63f8a06154ed
Estelle Recuero <estelle-ad@outlook.com>
2022-10-11 11:16:07 +0200

    clean eslint file

--- frontend/app/.eslintrc.js
+++ frontend/app/.eslintrc.js
@@ -1,52 +1,12 @@
-// module.exports = {
-//   parser: '@typescript-eslint/parser',
-//   parserOptions: {
-//     project: 'tsconfig.json',
-//     tsconfigRootDir: __dirname,
-//     sourceType: 'module',
-//     ecmaVersion: 2020,
-//     ecmaFeatures: {
-//       jsx: true, // Allows for the parsing of JSX
-//     },
-//   },
-//   settings: {
-//     react: {
-//       version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
-//     },
-//   },
-//   plugins: ['@typescript-eslint'],
-//   extends: [
-//     'plugin:@typescript-eslint/recommended',
-//     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-//     'plugin:prettier/recommended',
-//   ],
-//   root: true,
-//   env: {
-//     node: true,
-//     jest: true,
-//   },
-//   ignorePatterns: ['.eslintrc.js', '*spec.ts'],
-//   rules: {
-//     '@typescript-eslint/naming-convention': [
-//       'error',
-//       {
-//         selector: ['variable', 'parameter'],
-//         format: ['camelCase'],
-//       },
-//       {
-//         selector: ['function', 'interface', 'class'],
-//         format: ['PascalCase'],
-//       },
-//     ],
-//     '@typescript-eslint/no-unsafe-assignment': 'error',
-//   },
-// };
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -55,4 +15,18 @@ module.exports = {
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:@typescript-eslint/strict',
   ],
+  rules: {
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: ['variable', 'parameter'],
+        format: ['camelCase'],
+      },
+      {
+        selector: ['function', 'interface', 'class'],
+        format: ['PascalCase'],
+      },
+    ],
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+  },
 };
