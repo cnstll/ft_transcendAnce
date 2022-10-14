8f214959185927805ada4e83c74dd3b4eee7d03e
Estelle Recuero <estelle-ad@outlook.com>
2022-10-14 15:31:00 +0200

    added form handler functions for the search

--- frontend/app/src/components/section-components/search-box.tsx
+++ frontend/app/src/components/section-components/search-box.tsx
@@ -13,8 +13,14 @@ interface SearchBoxProps {
   channels?: Channel[],
 }
 
+const defaultSearchData = {
+  keyword: "",
+}
+
 function SearchBox({height, width, placeholder, users, channels}: SearchBoxProps) {
   const [isShown, setIsShown] = useState(false);
+  const [searchData, setSearchData] = useState(defaultSearchData);
+  const { keyword } = searchData;
 
   function ShowInfo() {
     setIsShown(true);
@@ -24,21 +30,40 @@ function SearchBox({height, width, placeholder, users, channels}: SearchBoxProps
     setIsShown(false);
   }
 
+  const ref = UseOutsideInputClick(ClickOutsideHandler);
+
   function FilterResults(channelItem: Channel) : boolean {
     if (channelItem.type !== 'DIRECTMESSAGE')
       return true;
     return false;
   }
 
-  const ref = UseOutsideInputClick(ClickOutsideHandler);
+  function OnChange(e: React.ChangeEvent<HTMLInputElement>) {
+      setSearchData((prevState) => ({
+        ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+    // Should instead send to Search API
+    console.log(searchData);
+  }
+
+  function OnSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // Should instead send to Search API
+    console.log(searchData);
+    setSearchData(defaultSearchData);
+  }
 
   return <>
         <div className="relative text-black">
-        <input className={height + width + " bg-white px-2 py-2 pr-6 rounded-lg text-[8px] sm:text-xs md:text-xs lg:text-sm focus:outline-none relative"}
-          type="text" name="search" placeholder={"Search " + placeholder} onFocus={ShowInfo} autoComplete="off" ref={ref}/>
-        <button type="submit" className="absolute top-1 sm:top-2.5 md:top-3 lg:top-4 right-2 text-[8px] sm:text-xs md:text-xs lg:text-sm text-black">
-            <FontAwesomeIcon icon={faMagnifyingGlass}/>
-        </button>
+          <form onSubmit={OnSubmit}>
+            <input className={height + width + " bg-white px-2 py-2 pr-6 rounded-lg text-[8px] sm:text-xs md:text-xs lg:text-sm focus:outline-none relative"}
+              type="text" name="search" id="keyword" value={keyword} onChange={OnChange}
+              onFocus={ShowInfo} ref={ref} autoComplete="off" placeholder={"Search " + placeholder}/>
+            <button type="submit" className="absolute top-1 sm:top-2.5 md:top-3 lg:top-4 right-2 text-[8px] sm:text-xs md:text-xs lg:text-sm text-black">
+                <FontAwesomeIcon icon={faMagnifyingGlass}/>
+            </button>
+          </form>
         <div className={"bg-white rounded-lg text-sm absolute " + width}>
         {isShown &&
         <ul>
