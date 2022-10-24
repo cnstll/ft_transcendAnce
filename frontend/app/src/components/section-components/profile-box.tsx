986c64a3ff24ca6f031c55331c3d891b9ab31584
COTHSC <thierryscully.colomban@gmail.com>
2022-10-24 15:52:26 +0200

    rm not neede enum

--- frontend/app/src/components/section-components/profile-box.tsx
+++ frontend/app/src/components/section-components/profile-box.tsx
@@ -5,13 +5,6 @@ interface UserOptionsProps {
   nickname: string | undefined,
 }
 
-export enum friendshipStatus {
-  REQUSTED,
-  ACCEPTED,
-  PENDING,
-  ADD
-}
-
 function ProfileBox(userProps: UserOptionsProps) {
 
   return <>
