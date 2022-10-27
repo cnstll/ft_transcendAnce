97cc913e5bca3b041983c5cd6adad7569f031e4c
Lea Lescure <lea.lescure@hotmail.fr>
2022-10-27 12:28:09 +0200

    bug: 401 error

--- frontend/app/src/components/section-components/two-factor-authentication.tsx
+++ frontend/app/src/components/section-components/two-factor-authentication.tsx
@@ -1,12 +1,56 @@
+import axios from 'axios';
 import { useState } from 'react';
 import { User } from '../global-components/interface';
+
+function ToggleOn() {
+  const disable2fa = () => {
+    axios
+      .post('http://localhost:3000/2fa/disable', {
+        withCredentials: true,
+      })
+      .then((res) => res)
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div
+      onClick={disable2fa}
+      className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full
+        peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300
+        after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+    >
+      <span className="ml-1 font-bold text-[9px]">ON</span>
+    </div>
+  );
+}
+
+function ToggleOff() {
+  const enable2fa = () => {
+    axios
+      .post('http://localhost:3000/2fa/generate', {
+        withCredentials: true,
+      })
+      .then((res) => res)
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div
+      onClick={enable2fa}
+      className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full
+        peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300
+        after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
+    >
+      <span className="ml-6 font-bold text-[9px] text-black">OFF</span>
+    </div>
+  );
+}
 
 function Toggle({
   twoAuthenticationIsSet,
 }: {
   twoAuthenticationIsSet: boolean | undefined;
 }) {
-  const [checked, setChecked] = useState(twoAuthenticationIsSet);
+  const [checked] = useState(twoAuthenticationIsSet);
 
   return (
     <div className="flex flex-col items-center justify-center overflow-hidden w-20">
@@ -18,22 +62,7 @@ function Toggle({
             checked={checked}
             readOnly
           />
-          <div
-            onClick={() => {
-              setChecked(!checked);
-            }}
-            className="w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-green-300  peer-checked:after:translate-x-full
-            peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300
-            after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"
-          >
-            {checked ? (
-              <span className="ml-1 font-bold text-[9px] uppercase">ON</span>
-            ) : (
-              <span className="ml-6 font-bold text-[9px] uppercase text-black">
-                OFF
-              </span>
-            )}
-          </div>
+          {checked ? <ToggleOn /> : <ToggleOff />}
         </label>
       </div>
     </div>
