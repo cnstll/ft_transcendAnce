247523775d34f58da50eb8f66d4b0ec2fc20d0af
cnstll <67589684+cnstll@users.noreply.github.com>
2022-11-04 23:28:13 +0100

    fixing linting errors on '/'

--- frontend/app/src/components/section-components/chat/create-channel-form.tsx
+++ frontend/app/src/components/section-components/chat/create-channel-form.tsx
@@ -24,7 +24,7 @@ function validateNameInput(input: string): boolean {
 
 function validatePwdInput(input: string): boolean {
     // Regex would accept alphanumeric and special char: !?@#$%^&*()+./'"" but no space
-  const regex = /^[!?@#$%^&*()+.'"\/\w\d]+$/;
+  const regex = /^[!?@#$%^&*()+.'"/\w\d]+$/;
   const regexWhiteChar = /^\s+$/;
   const ret = input.length !== 0 && input.length <= 32 && regex.test(input)
     && !regexWhiteChar.test(input)
