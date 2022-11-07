0ae311bcf7786e9a695d64169a75eb28005b1a48
cnstll <calle@student.42.fr>
2022-11-07 12:50:03 +0100

    Implementing a switch-case block instead of if-elif-else

--- frontend/app/src/components/custom-hooks/emit-join-channel.tsx
+++ frontend/app/src/components/custom-hooks/emit-join-channel.tsx
@@ -2,21 +2,25 @@ import { Channel, channelType } from '../global-components/interface';
 import { socket } from '../global-components/client-socket';
 
 function JoinChannel(channelInfo: Channel) {
-  if (channelInfo.type === channelType.Public) {
-    socket.emit('joinRoom', {
-      joinInfo: {
-        id: channelInfo.id,
-      },
-    });
-  } else if (channelInfo.type === channelType.Protected) {
-    socket.emit('joinRoom', {
-      joinInfo: {
-        id: channelInfo.id,
-        passwordHash: channelInfo.passwordHash,
-      },
-    });
-  } else {
-    console.log('NOT IMPLEMENTED ATM');
+  switch (channelInfo.type) {
+    case channelType.Public:
+      socket.emit('joinRoom', {
+        joinInfo: {
+          id: channelInfo.id,
+        },
+      });
+      break;
+    case channelType.Protected:
+      socket.emit('joinRoom', {
+        joinInfo: {
+          id: channelInfo.id,
+          passwordHash: channelInfo.passwordHash,
+        },
+      });
+      break;
+    default:
+      console.log('NOT IMPLEMENTED ATM');
+      break;
   }
 }
 export default JoinChannel;
