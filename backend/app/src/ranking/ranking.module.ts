ee763c8667c6bf84fd9542290864d42d092c7bc9
Lea Lescure <lea.lescure@hotmail.fr>
2022-11-08 11:32:17 +0100

    feat(ranking): instant leaderboard done

--- backend/app/src/ranking/ranking.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from 'src/user/user.module';
-import { RankingService } from './raking.service';
-
-@Module({
-  providers: [RankingService],
-  imports: [UserModule],
-})
-export class RankingModule {}
--- frontend/app/src/components/global-components/ranking.tsx
+++ frontend/app/src/components/global-components/ranking.tsx
@@ -4,61 +4,70 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import Background from '../section-components/background';
 import OneBox from '../section-components/one-box';
-// import { NumericFormat } from 'react-number-format';
+import { NumericFormat } from 'react-number-format';
 import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import useUserInfo from '../query-hooks/useUserInfo';
-// import { RankingData } from './interface';
+import { RankingData } from './interface';
+import useLeaderboard from '../query-hooks/useLeaderboard';
 
-// function RankingList({ position, image, name, score }: RankingData) {
-//   return (
-//     <tr className="h-20">
-//       {position === 1 ? (
-//         <td
-//           className="flex justify-center pt-5
-//                     text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
-//         >
-//           🏆
-//         </td>
-//       ) : (
-//         <td className="flex justify-center pt-5 text-lg sm:text-xl md:text-2xl lg:text-3xl">
-//           {position}
-//         </td>
-//       )}
-//       <td>
-//         <img
-//           className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
-//           src={image}
-//           alt="Rounded avatar"
-//         />
-//       </td>
-//       <td>{name}</td>
-//       <td className="flex justify-center">
-//         <NumericFormat
-//           className="text-lg sm:text-lg md:text-xl lg:text-2xl"
-//           value={score}
-//           thousandSeparator=" "
-//           displayType="text"
-//         />
-//       </td>
-//     </tr>
-//   );
-// }
+interface RankingDataProps {
+  rankingData: RankingData;
+  position: number;
+}
+
+function RankingList({ rankingData, position }: RankingDataProps) {
+  return (
+    <tr className="h-20">
+      {position === 1 ? (
+        <td
+          className="flex justify-center pt-5
+                    text-2xl sm:text-3xl md:text-4xl lg:text-5xl"
+        >
+          🏆
+        </td>
+      ) : (
+        <td className="flex justify-center pt-5 text-lg sm:text-xl md:text-2xl lg:text-3xl">
+          {position}
+        </td>
+      )}
+      <td>
+        <img
+          className="w-8 h-8 sm:w-9 sm:h-9 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14 rounded-full"
+          src={rankingData.avatarImg}
+          alt="Rounded avatar"
+        />
+      </td>
+      <td>{rankingData.nickname}</td>
+      <td className="flex justify-center">
+        <NumericFormat
+          className="text-lg sm:text-lg md:text-xl lg:text-2xl"
+          value={rankingData.eloScore}
+          thousandSeparator=" "
+          displayType="text"
+        />
+      </td>
+    </tr>
+  );
+}
 
 function Ranking() {
   const user = useUserInfo();
+  const leaderboard = useLeaderboard();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user.isError) navigate('/sign-in');
+    void leaderboard.refetch();
   });
 
   return (
     <>
-      {user.isError && (
-        <p className="text-base text-gray-400">We encountered an error 🤷</p>
-      )}
-      {user.isSuccess && (
+      {user.isError ||
+        (leaderboard.isError && (
+          <p className="text-base text-gray-400">We encountered an error 🤷</p>
+        ))}
+      {user.isSuccess && leaderboard.isSuccess && (
         <div>
           <Background background={BackgroundGeneral}>
             <Navbar
@@ -81,16 +90,15 @@ function Ranking() {
                       </td>
                     </tr>
                   </thead>
-                  {/* <tbody>
-                {rankingExamples.map((rankingExample) => (
-                  <RankingList
-                    position={rankingExample.position}
-                    image={rankingExample.image}
-                    name={rankingExample.name}
-                    score={rankingExample.score}
-                  />
-                ))}
-              </tbody> */}
+                  <tbody>
+                    {leaderboard.data.map((user, index) => (
+                      <RankingList
+                        key={user.id}
+                        rankingData={user}
+                        position={index + 1}
+                      />
+                    ))}
+                  </tbody>
                 </table>
               </OneBox>
             </div>
