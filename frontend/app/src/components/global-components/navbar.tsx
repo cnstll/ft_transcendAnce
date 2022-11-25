8793560255b564b734dfc5bbbcba524e5acae1de
Lea Lescure <lea.lescure@hotmail.fr>
2022-11-25 15:47:40 +0100

    feat(toastify): change tailwind css

--- frontend/app/src/components/global-components/navbar.tsx
+++ frontend/app/src/components/global-components/navbar.tsx
@@ -20,52 +20,38 @@ interface BannerProps {
   avatarImg: string;
 }
 
-function Accept({onAccept, name}: {onAccept: () => void, name: string} ) {
-  const handleAccept= () => {
+function Accept({ onAccept, name }: { onAccept: () => void; name: string }) {
+  const handleAccept = () => {
     onAccept();
   };
+
   return (
-    <div >
-      <h3>
-        {name[0].toUpperCase() + name.substring(1)} wants to play. 
-      </h3>
-        <div className='flex flex-row-reverse'>
-        <button onClick={handleAccept} className="
-        rounded-l
-        px-6
-        py-2
-        border-2 border-blue-600
-        text-blue-600
-        font-medium
-        text-xs
-        leading-tight
-        uppercase
-        hover:bg-black hover:bg-opacity-5
-        focus:outline-none focus:ring-0
-        transition
-        duration-150
-        ease-in-out
-          ">Accept</button>    <button className='
-        rounded-l
-        px-6
-        py-2
-        border-2 border-blue-600
-        text-blue-600
-        font-medium
-        text-xs
-        leading-tight
-        uppercase
-        hover:bg-black hover:bg-opacity-5
-        focus:outline-none focus:ring-0
-        transition
-        duration-150
-        ease-in-out
-      '>Refuse</button>
+    <div className="p-4 text-gray-500 bg-white">
+      <div className="flex">
+        <div className="ml-3 text-sm font-normal">
+          <span className="mb-1 text-sm font-semibold text-gray-900">
+            {name[0].toUpperCase() + name.substring(1)} wants to play.
+          </span>
+          <div className="grid grid-cols-2 gap-2">
+            <div>
+              <button
+                onClick={handleAccept}
+                className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue rounded-lg"
+              >
+                Accept
+              </button>
+            </div>
+            <div>
+              <button className="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg">
+                Not now
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
 
 function Navbar({ text, avatarImg }: BannerProps) {
   const [isShown, setIsShown] = useState(false);
@@ -78,7 +64,6 @@ function Navbar({ text, avatarImg }: BannerProps) {
   const navigate = useNavigate();
   const channelUsersQueryKey = 'channelUsers';
 
-
   const showInfo = () => {
     setIsShown((current) => !current);
   };
@@ -86,7 +71,6 @@ function Navbar({ text, avatarImg }: BannerProps) {
   function ClickOutsideHandler() {
     setIsShown(false);
   }
-
 
   const ref = UseOutsideClick(ClickOutsideHandler);
   useEffect(() => {
@@ -110,58 +94,58 @@ function Navbar({ text, avatarImg }: BannerProps) {
       void queryClient.invalidateQueries(channelUsersQueryKey);
     });
 
-
-  function accept() {
-    acceptInvite = true;
-  }
-
-  const notify = (challenger: User) => toast(<Accept onAccept={accept} name={challenger.nickname}  />,{
-    position: 'bottom-right',
-    onClose: () => {
-      if (acceptInvite){
-        socket.emit('acceptInvite', challenger);
-        navigate('/play');
-      } else {
-        socket.emit('refuseInvite', challenger);
-      }
+    function accept() {
+      acceptInvite = true;
     }
-  });
 
-  const inviteListener = (challenger: User) => {
-    acceptInvite = false;
-    notify(challenger);
-  };
-
-
-      socket.on('invitedToGame', inviteListener);
-
-      socket.on('inviteRefused', (): void => {
-      toast('go fuck yourself' , {
+    const notify = (challenger: User) =>
+      toast.info(<Accept onAccept={accept} name={challenger.nickname} />, {
         position: 'bottom-right',
+        icon: '🏓',
+        onClose: () => {
+          if (acceptInvite) {
+            socket.emit('acceptInvite', challenger);
+            navigate('/play');
+          } else {
+            socket.emit('refuseInvite', challenger);
+          }
+        },
+      });
+
+    const inviteListener = (challenger: User) => {
+      acceptInvite = false;
+      notify(challenger);
+    };
+
+    socket.on('invitedToGame', inviteListener);
+
+    socket.on('inviteRefused', (): void => {
+      toast.info('Sorry, nobody wants to play with you...', {
+        position: 'bottom-right',
+        icon: '😭',
         autoClose: 1000,
         onClose: () => navigate('/'),
       });
-      });
+    });
 
-      return () => {
-        socket.off('userDisconnected');
-        socket.off('userConnected');
-        socket.off('userInGame');
-        socket.off('userGameEnded');
-        socket.off('invitedToGame');
-        socket.off('inviteRefused');
-      };
-    }, []);
+    return () => {
+      socket.off('userDisconnected');
+      socket.off('userConnected');
+      socket.off('userInGame');
+      socket.off('userGameEnded');
+      socket.off('invitedToGame');
+      socket.off('inviteRefused');
+    };
+  }, []);
 
-
-
-    return (
+  return (
+    <>
       <div className="flex flex-row px-2 sm:px-2 md:px-5 lg:px-8 py-5 justify-between items-center">
         <Link to="/">
           <h2
             className={`${
-typeof text === 'string' ? 'text-[8px] ' : 'text-[18px] '
-} sm:text-xl md:text-4xl lg:text-5xl text-white font-bold`}
+              typeof text === 'string' ? 'text-[8px] ' : 'text-[18px] '
+            } sm:text-xl md:text-4xl lg:text-5xl text-white font-bold`}
           >
             {text}
           </h2>
@@ -171,14 +155,14 @@ typeof text === 'string' ? 'text-[8px] ' : 'text-[18px] '
           width="w-24 sm:w-36 md:w-40 lg:w-56 xl:w-56 "
           placeholder="player"
           users={usersData.data}
-          />
+        />
         <div className="relative" ref={ref}>
           <div className="text-sm sm:text-xl md:text-2xl lg:text-3xl flex flex-row gap-2">
             <img
               className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
               src={avatarImg}
               alt="Rounded avatar"
-              />
+            />
             <button onClick={showInfo} className="text-white font-bold">
               <FontAwesomeIcon icon={faChevronDown} />
             </button>
@@ -191,40 +175,44 @@ typeof text === 'string' ? 'text-[8px] ' : 'text-[18px] '
             </div>
           )}
         </div>
-        <ToastContainer closeButton={false}/>
       </div>
-    );
-  }
+      <ToastContainer closeButton={false} />
+    </>
+  );
+}
 
-    function UserInfo() {
+function UserInfo() {
   function logoutHandler() {
     socket.emit('disconnectUser');
-  axios
-    .get(`${apiUrl}/user/logout`, {
-      withCredentials: true,
-    })
-    .catch((error) => console.log(error));
-}
-return (
-  <>
-    <div>
-      <Link to="/profile">
-        <p className="text-center hover:underline my-2">Profile</p>
-      </Link>
-      <Link to="/chat">
-        <p className="text-center hover:underline my-2">Chat</p>
-      </Link>
-      <Link to="/ranking">
-        <p className="text-center hover:underline my-2">Ranking</p>
-      </Link>
-      <Link to="/sign-in">
-        <p className="text-center hover:underline my-2" onClick={logoutHandler}>
-          Log out
-        </p>
-      </Link>
-    </div>
+    axios
+      .get(`${apiUrl}/user/logout`, {
+        withCredentials: true,
+      })
+      .catch((error) => console.log(error));
+  }
+  return (
+    <>
+      <div>
+        <Link to="/profile">
+          <p className="text-center hover:underline my-2">Profile</p>
+        </Link>
+        <Link to="/chat">
+          <p className="text-center hover:underline my-2">Chat</p>
+        </Link>
+        <Link to="/ranking">
+          <p className="text-center hover:underline my-2">Ranking</p>
+        </Link>
+        <Link to="/sign-in">
+          <p
+            className="text-center hover:underline my-2"
+            onClick={logoutHandler}
+          >
+            Log out
+          </p>
+        </Link>
+      </div>
     </>
-);
+  );
 }
 
 export default Navbar;
