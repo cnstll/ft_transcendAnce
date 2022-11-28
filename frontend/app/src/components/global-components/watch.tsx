ba0094e6665ed83b205628e0d7bb160bbd56ed35
cnstll <calle@student.42.fr>
2022-11-28 11:34:28 +0100

    Displaying corresponding win/lose screen for watch part of game

--- frontend/app/src/components/global-components/watch.tsx
+++ frontend/app/src/components/global-components/watch.tsx
@@ -5,19 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import Background from '../section-components/background';
 import { useNavigate, useParams } from 'react-router-dom';
-import { useEffect} from 'react';
+import { useEffect } from 'react';
 import useUserInfo from '../query-hooks/useUserInfo';
-import WatchGame from '../section-components/play/watchGame';
+import WatchGame from '../section-components/game/watch-game';
 
 function Watch() {
   const user = useUserInfo();
-  const {playerId} = useParams();
+  const { playerId } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (user.isError) navigate('/sign-in');
   }, []);
-
 
   console.log('this is playerId: ', playerId);
   if (user.isSuccess)
@@ -29,10 +28,7 @@ function Watch() {
             avatarImg={user.data.avatarImg}
           />
           {playerId != null && (
-          <WatchGame 
-              avatarImg={user.data.avatarImg}
-              userId={user.data.id}
-          />
+            <WatchGame avatarImg={user.data.avatarImg} userId={user.data.id} />
           )}
         </Background>
       </div>
--- frontend/app/src/components/section-components/game/watch-game.tsx
+++ frontend/app/src/components/section-components/game/watch-game.tsx
@@ -3,11 +3,14 @@ import { useEffect, useRef, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useOpponentInfo } from 'src/components/query-hooks/useTargetInfo';
 import { socket } from '../../global-components/client-socket';
-import { GameCoords, GameInformation, GameStatus } from '../../global-components/interface';
+import {
+  GameCoords,
+  GameInformation,
+  GameStatus,
+} from '../../global-components/interface';
 import { GameInfo } from '../../../proto/file_pb';
 import { cacheCanvas, draw } from 'src/components/custom-hooks/draw';
 import { drawGameStatus } from 'src/components/custom-hooks/game-statuses';
-
 
 const gameConstants = {
   relativeGameWidth: 1000,
@@ -38,25 +41,25 @@ function PlayerAvatar({
           <img
             className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
             src={currentPlayerAvatar}
-            />
+          />
           <img
             className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
             src={opponentAvatar}
-            />
+          />
         </div>
       ) : (
-          <div className="flex flex-row justify-between mt-4">
-            <img
-              className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
-              src={opponentAvatar}
-              />
-            <img
-              className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
-              src={currentPlayerAvatar}
-              />
-          </div>
-        )}
-      </>
+        <div className="flex flex-row justify-between mt-4">
+          <img
+            className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
+            src={opponentAvatar}
+          />
+          <img
+            className="w-10 h-10 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 rounded-full"
+            src={currentPlayerAvatar}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
@@ -65,8 +68,8 @@ const gameCoordinates: GameCoords = {
   p2y: 0,
   bx: 0,
   by: 0,
-  p1s: 0, 
-  p2s: 0, 
+  p1s: 0,
+  p2s: 0,
 };
 
 const gameInfo: GameInformation = {
@@ -87,8 +90,13 @@ const gameInfo: GameInformation = {
   playerNumber: 1,
 };
 
-function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
-
+function WatchGame({
+  avatarImg,
+  userId,
+}: {
+  avatarImg: string;
+  userId: string;
+}) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const navigate = useNavigate();
   const { playerId } = useParams();
@@ -99,9 +107,9 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
   useEffect(() => {
     socket.emit(
       'watchGame',
-      {playerId: playerId},
+      { playerId: playerId },
       (response: { playerNumber: number }) => {
-        console.log('here i am the server responded')
+        console.log('here i am the server responded');
         gameInfo.playerNumber = response.playerNumber;
       },
     );
@@ -110,7 +118,6 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
   useEffect(() => {
     gameInfo.canvas = canvasRef.current;
     if (gameInfo.canvas !== null) {
-
       const joinListener = (text: {
         gameId: string;
         status: string;
@@ -125,8 +132,7 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
           setGameStatus(GameStatus.DONE);
           navigate('/profile');
         } else if (text.status === 'OVER') {
-          // setPlayerOneScore(text.player1score);
-
+          gameInfo.playerOneScore = text.player1score;
           setGameStatus(GameStatus.OVER);
         } else if (text.status === 'PLAYING') {
           setGameStatus(GameStatus.PLAYING);
@@ -142,12 +148,16 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
       // Setting the numbers needed to scale the game based on window size
       gameInfo.heightScalar =
         window.innerHeight / (2 * gameConstants.relativeGameHeight);
-      gameInfo.widthScalar = window.innerWidth / gameConstants.relativeGameWidth;
+      gameInfo.widthScalar =
+        window.innerWidth / gameConstants.relativeGameWidth;
       gameInfo.paddleWidth = gameInfo.widthScalar * gameConstants.paddleWidth;
-      gameInfo.p1x = gameInfo.widthScalar * gameConstants.player1PaddlePosX - gameInfo.paddleWidth;
+      gameInfo.p1x =
+        gameInfo.widthScalar * gameConstants.player1PaddlePosX -
+        gameInfo.paddleWidth;
       gameInfo.p2x = gameInfo.widthScalar * gameConstants.player2PaddlePosX;
       gameInfo.ballWidth = gameInfo.widthScalar * gameConstants.ballHeight;
-      gameInfo.paddleHeight = gameInfo.heightScalar * gameConstants.paddleHeight;
+      gameInfo.paddleHeight =
+        gameInfo.heightScalar * gameConstants.paddleHeight;
 
       gameInfo.context = gameInfo.canvas.getContext('2d');
 
@@ -157,8 +167,7 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
         gameInfo.canvas.style.width = `${window.innerWidth}px`;
         gameInfo.canvas.style.height = `${window.innerHeight / 2}px`;
 
-
-        gameInfo.cacheCanvas = cacheCanvas(gameInfo); 
+        gameInfo.cacheCanvas = cacheCanvas(gameInfo);
 
         // this size is used to have a relative font size
         // const size = 0.03 * window.innerWidth;
@@ -169,8 +178,9 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
 
         const messageListener = (encoded: Uint8Array) => {
           try {
-            gameInfo.gameCoordinates = GameInfo.deserializeBinary(encoded).toObject();
-            window.requestAnimationFrame(function() {
+            gameInfo.gameCoordinates =
+              GameInfo.deserializeBinary(encoded).toObject();
+            window.requestAnimationFrame(function () {
               draw(gameInfo);
             });
           } catch (e) {
@@ -202,7 +212,7 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
           <canvas
             ref={canvasRef}
             className="border-solid border-2 border-white"
-            />
+          />
         )}
         {gameStatus === GameStatus.DONE && (
           <p className="text-white"> The game is over, you probably lost </p>
@@ -211,19 +221,19 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
           <canvas
             ref={canvasRef}
             className="border-solid border-2 border-white"
-            />
+          />
         )}
         {gameStatus === GameStatus.PENDING && (
           <canvas
             ref={canvasRef}
             className="border-solid border-2 border-white"
-            />
+          />
         )}
         {gameStatus === GameStatus.PAUSED && (
           <canvas
             ref={canvasRef}
             className="border-solid border-2 border-white"
-            />
+          />
         )}
         {gameStatus === GameStatus.PLAYING &&
           opponent.isSuccess &&
@@ -232,7 +242,7 @@ function WatchGame({ avatarImg, userId }: {avatarImg: string, userId: string}) {
               currentPlayerAvatar={avatarImg}
               opponentAvatar={opponent.data.avatarImg}
               playerNumber={gameInfo.playerNumber}
-              />
+            />
           )}
       </div>
     </div>
