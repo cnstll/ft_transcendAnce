e3037ebf0e757c8a00aa9fe91a023f293028c4db
Estelle Recuero <estelle-ad@outlook.com>
2022-11-28 17:33:40 +0100

    added in front icon for type of channels in the search

--- frontend/app/src/components/section-components/chat/search-channel-item.tsx
+++ frontend/app/src/components/section-components/chat/search-channel-item.tsx
@@ -3,6 +3,12 @@ import { Channel, channelType } from '../../global-components/interface';
 import JoinChannel from '../../custom-hooks/emit-join-channel';
 import PasswordModal from './password-modal';
 import { useState } from 'react';
+import {
+  faLock,
+  faEnvelope,
+  faEnvelopeCircleCheck
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function SearchChannelItem({ channel }: { channel: Channel }) {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -20,7 +26,20 @@ function SearchChannelItem({ channel }: { channel: Channel }) {
   return (
     <>
       <Link to={`../chat/${channel.id}`} onClick={onClick}>
-        <li className="p-4">{channel.name}</li>
+        <div className='flex justify-start p-2'>
+          <div>
+          {channel.type === channelType.Protected && (
+            <FontAwesomeIcon className="text-md text-purple-medium" icon={faLock} />
+          )}
+          {channel.type === channelType.Private && (
+            <FontAwesomeIcon className="text-md text-purple-medium" icon={faEnvelopeCircleCheck} />
+          )}
+          {channel.type === channelType.Private && (
+            <FontAwesomeIcon className="text-md text-red-500" icon={faEnvelope} />
+          )}
+          </div>
+          <li className="px-2">{channel.name}</li>
+        </div>
       </Link>
       {showModal &&
         <PasswordModal setShowModal={setShowModal} channel={channel}
