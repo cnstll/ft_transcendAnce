f321579fff6c93a50d69eeaa59f920ef17b91e3d
Estelle Recuero <estelle-ad@outlook.com>
2022-11-29 13:46:53 +0100

    added toastify to notify when not invited to a channel

--- frontend/app/src/components/section-components/chat/search-channel-item.tsx
+++ frontend/app/src/components/section-components/chat/search-channel-item.tsx
@@ -9,15 +9,19 @@ import {
   faEnvelopeCircleCheck
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { toast, ToastContainer } from 'react-toastify';
 
 function SearchChannelItem({ channel, invited }: { channel: Channel, invited: boolean }) {
   const [showModal, setShowModal] = useState<boolean>(false);
+  const customToastId = "custom-toast-chan-not-invited";
 
   function onClick(e: React.MouseEvent) {
     e.preventDefault();
     if (channel.type === channelType.Private && !invited)
-    /** replace with a toastify notification */
-      alert("You're not invited to this channel");
+      toast.error("You're not invited to this channel", {
+        toastId: customToastId,
+        position: toast.POSITION.TOP_LEFT
+      });
     else if (channel.type === channelType.Protected) {
       setShowModal(true);
     }
@@ -31,7 +35,7 @@ function SearchChannelItem({ channel, invited }: { channel: Channel, invited: bo
   return (
     <>
       <Link to={`../chat/${channel.id}`} onClick={onClick}>
-        <div className='flex justify-start p-2'>
+        <div className='flex justify-start px-2 py-3'>
           <li className="px-2 flex-wrap break-words">
             {channel.type === channelType.Protected && (
               <FontAwesomeIcon className="text-md text-purple-medium pr-1" icon={faLock} />
@@ -40,10 +44,11 @@ function SearchChannelItem({ channel, invited }: { channel: Channel, invited: bo
               <FontAwesomeIcon className="text-md text-purple-medium pr-1" icon={faEnvelopeCircleCheck} />
             )}
             {!invited && channel.type === channelType.Private && (
-              <FontAwesomeIcon className="text-md text-red-500 pr-1" icon={faEnvelope}/>
+              <FontAwesomeIcon className="text-md text-gray-400 pr-1" icon={faEnvelope}/>
             )}
             {channel.name} </li>
         </div>
+        <ToastContainer closeButton={false}/>
       </Link>
       {showModal &&
         <PasswordModal setShowModal={setShowModal} channel={channel}
