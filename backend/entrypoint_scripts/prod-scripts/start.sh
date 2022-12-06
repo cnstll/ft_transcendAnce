761f38f67bd909a5fca8ab7b88fce3d69761b490
COTHSC <thierryscully.colomban@gmail.com>
2022-12-06 13:54:28 +0100

    minimalist prod build for correction WIP

--- backend/entrypoint_scripts/prod-scripts/start.sh
+++ backend/entrypoint_scripts/prod-scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /home/entrypoint_scripts/wait-for-it.sh  db:5432
 npm ci;
-npx prisma migrate reset -f && npm run build && npx dotenv -e .env.prod -- npm run start:prod;
+npm run build && npx dotenv -e .env.prod -- npm run start:prod;
