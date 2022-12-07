4d876f858cecec908e64989ebf8b6c3a4173302b
cnstll <67589684+cnstll@users.noreply.github.com>
2022-12-07 17:06:05 +0100

    Update getChannelInvites.tsx

--- frontend/app/src/components/query-hooks/getChannelInvites.tsx
+++ frontend/app/src/components/query-hooks/getChannelInvites.tsx
@@ -30,7 +30,7 @@ UseQueryResult<User[] | undefined> {
 
 const fetchChannelInvites = () =>
 axios
-  .get<{id: string}[]>('${apiUrl}/user/get-channel-invites', {
+  .get<{id: string}[]>(`${apiUrl}/user/get-channel-invites`, {
     withCredentials: true,
   })
   .then((res) => res.data);
