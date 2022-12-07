1c1ea498f6b99b8a79d182239463505a643d29fc
COTHSC <thierryscully.colomban@gmail.com>
2022-12-07 13:54:22 +0000

    add manual seeding to the db so achievements work properly and the site has some users

--- backend/entrypoint_scripts/prod-scripts/start.sh
+++ backend/entrypoint_scripts/prod-scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /home/entrypoint_scripts/wait-for-it.sh  db:5432
 npm ci;
-npx prisma migrate deploy && npm run build && npm run start:prod;
+npx prisma migrate deploy && npx prisma db seed && npm run build && npm run start:prod;
